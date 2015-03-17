cc9012a28d3d23e3d85ed9fb03d65d8b692ff06e
Xavier Goás Aguililla <xavier.goas@gmail.com>
2015-03-17 18:52:56 +0100

    stuff fixed

---
+++ code/contiki/antenna_off_idle/obj_avr-zigduino/leds.d
@@ -0,0 +1,15 @@
+obj_avr-zigduino/leds.o: \
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/dev/leds.c \
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/dev/leds.h \
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//platform/avr-zigduino/contiki-conf.h \
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/sys/clock.h \
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/sys/energest.h \
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/sys/rtimer.h \
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//cpu/avr/rtimer-arch.h
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/dev/leds.c :
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/dev/leds.h :
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//platform/avr-zigduino/contiki-conf.h :
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/sys/clock.h :
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/sys/energest.h :
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/sys/rtimer.h :
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//cpu/avr/rtimer-arch.h :
