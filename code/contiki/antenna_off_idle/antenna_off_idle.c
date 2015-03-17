ca4a123ccfc8c8aec5bbd25e56805723f3652007
Xavier Goás Aguililla <xavier.goas@gmail.com>
2015-03-17 18:14:17 +0100

    antenna off and on stuff done

--- code/contiki/antenna_off_idle/antenna_off_idle.c
+++ code/contiki/antenna_off_idle/antenna_off_idle.c
@@ -2,16 +2,35 @@
 #include "contiki.h"
 #include "net/netstack.h"
 
-/* #include <stdio.h> /\* For print
 /*---------------------------------------------------------------------------*/
 PROCESS(antenna_off_idle_process, "antenna off, idle");
 AUTOSTART_PROCESSES(&antenna_off_idle_process);
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(antenna_off_idle_process, ev, data)
 {
+#define AMOUNT_OF_SAMPLES 10
+  
+  static struct etimer et;
   PROCESS_BEGIN();
 
-  NETSTACK_RADIO.off(0); //radios off
+  // initialise pin
+  DDRE |= _BV(PE6);
+  
+  uint32_t sampling_round;
+  for (sampling_round = 0; sampling_round < AMOUNT_OF_SAMPLES; sampling_round++) {
+    // flip pin
+    PORTE ^= _BV(PE6);
+  
+    NETSTACK_MAC.off(0); //mac off
+    NETSTACK_RADIO.off(); //radios off
+
+    etimer_set(&et, (CLOCK_SECOND * 10));
+
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+  
+    // flip pin back
+    PORTE ^= _BV(PE6);
+  }
 
   PROCESS_END();
 }
--- code/contiki/antenna_on_idle/antenna_on_idle.c
+++ code/contiki/antenna_on_idle/antenna_on_idle.c
@@ -8,9 +8,25 @@ AUTOSTART_PROCESSES(&antenna_on_idle_process);
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(antenna_on_idle_process, ev, data)
 {
+  static struct etimer et;
+  
   PROCESS_BEGIN();
 
-  /* NETSTACK_RADIO.on(); //radios on */
+  // initialise pin
+  DDRE |= _BV(PE6);
+  
+  uint32_t sampling_round;
+  for (sampling_round = 0; sampling_round < AMOUNT_OF_SAMPLES; sampling_round++) {
+    // flip pin
+    PORTE ^= _BV(PE6);
+  
+    etimer_set(&et, (CLOCK_SECOND * 10));
+
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+  
+    // flip pin back
+    PORTE ^= _BV(PE6);
+  }
 
   PROCESS_END();
 }
