0b71baab5699aeccab9fa17380bd9d1334ca7c0a
warreee <wardschodts@hotmail.com>
2015-03-17 18:20:11 +0100

    deployment

--- code/contiki/antenna_on_idle/obj_avr-zigduino/ifft.d
+++
@@ -1,7 +0,0 @@
-obj_avr-zigduino/ifft.o: \
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/lib/ifft.c \
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/lib/ifft.h \
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//platform/avr-zigduino/contiki-conf.h
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/lib/ifft.c :
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/lib/ifft.h :
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//platform/avr-zigduino/contiki-conf.h :
