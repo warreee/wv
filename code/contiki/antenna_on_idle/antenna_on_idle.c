5ea1071990816c46e5778fe597328f755cbb7cff
warreee <wardschodts@hotmail.com>
2015-03-24 18:41:05 +0100

    tussentijdsverslag

--- code/contiki/antenna_on_idle/antenna_on_idle.c
+++ code/contiki/antenna_on_idle/antenna_on_idle.c
@@ -9,7 +9,6 @@ AUTOSTART_PROCESSES(&antenna_on_idle_process);
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(antenna_on_idle_process, ev, data)
 {
-#define AMOUNT_OF_SAMPLES 10
   
   static struct etimer et;
   
@@ -17,18 +16,16 @@ PROCESS_THREAD(antenna_on_idle_process, ev, data)
 
   // initialise pin
   DDRE |= _BV(PE6);
-  
-  uint32_t sampling_round;
-  for (sampling_round = 0; sampling_round < AMOUNT_OF_SAMPLES; sampling_round++) {
-    // flip pin
-    PORTE ^= _BV(PE6);
+  while (1) {
+// Pin Flip
+PORTE ^= _BV(PE6);
   
     etimer_set(&et, (CLOCK_SECOND * 10));
 
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
   
-    // flip pin back
-    PORTE ^= _BV(PE6);
+// Pin Flip
+PORTE ^= _BV(PE6);
   }
 
   PROCESS_END();
