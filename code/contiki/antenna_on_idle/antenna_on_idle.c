de93bd858076162fe7659615506b09a68f143e51
warreee <wardschodts@hotmail.com>
2015-03-29 18:47:47 +0200

    tussentijdsverslag

--- code/contiki/antenna_on_idle/antenna_on_idle.c
+++ code/contiki/antenna_on_idle/antenna_on_idle.c
@@ -17,10 +17,11 @@ PROCESS_THREAD(antenna_on_idle_process, ev, data)
   // initialise pin
   DDRE |= _BV(PE6);
   
-  uint32_t sampling_round;
-  for (sampling_round = 0; sampling_round < AMOUNT_OF_SAMPLES; sampling_round++) {
-    // flip pin
-    PORTE ^= _BV(PE6);
+
+  while(1)
+  {
+    // Pin Flip
+    PORTE ^= _BV(PE6);  
   
     etimer_set(&et, (CLOCK_SECOND * 0.5));
 
