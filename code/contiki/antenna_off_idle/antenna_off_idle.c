cad42bd31eb1bf5dcfa79c27ff31730c3c5a2535
warreee <wardschodts@hotmail.com>
2015-04-07 14:43:54 +0200

    new experiments

--- code/contiki/antenna_off_idle/antenna_off_idle.c
+++ code/contiki/antenna_off_idle/antenna_off_idle.c
@@ -1,5 +1,4 @@
 
-
 #include "contiki.h"
 #include "net/netstack.h"
 #include <avr/io.h>
@@ -10,9 +9,9 @@ AUTOSTART_PROCESSES(&antenna_off_idle_process);
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(antenna_off_idle_process, ev, data)
 {
-#define AMOUNT_OF_SAMPLES 20
-  
+
   static struct etimer et;
+
   PROCESS_BEGIN();
 
   // initialise pin
@@ -21,18 +20,17 @@ PROCESS_THREAD(antenna_off_idle_process, ev, data)
   NETSTACK_MAC.off(0); //mac off
   NETSTACK_RADIO.off(); //radios off
     
-  uint32_t sampling_round;
-  for (sampling_round = 0; sampling_round < AMOUNT_OF_SAMPLES; sampling_round++) {
-    // flip pin
-    PORTE ^= _BV(PE6);
+  while(1)
+  {
+    // Pin Flip
+    PORTE ^= _BV(PE6);  
   
     etimer_set(&et, (CLOCK_SECOND * 0.5));
 
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
   }
 
-  // pin low
-  PORTE &= ~(_BV(PE6));
+
 
   PROCESS_END();
 }
