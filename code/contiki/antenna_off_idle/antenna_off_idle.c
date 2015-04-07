d80b3f941ff2c50dda32b5c98285a4dcb0dbefbb
warreee <wardschodts@hotmail.com>
2015-04-07 15:04:48 +0200

    antenna again

--- code/contiki/antenna_off_idle/antenna_off_idle.c
+++ code/contiki/antenna_off_idle/antenna_off_idle.c
@@ -25,7 +25,7 @@ PROCESS_THREAD(antenna_off_idle_process, ev, data)
     // Pin Flip
     PORTE ^= _BV(PE6);  
   
-    etimer_set(&et, (CLOCK_SECOND * 0.5));
+    etimer_set(&et, (CLOCK_SECOND * 2));
 
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
   }
