3e64ca10e3f3469ead73dd2723a9c204e5912901
Xavier Goás Aguililla <xavier.goas@gmail.com>
2015-04-27 15:57:36 +0200

    idle receiver component

--- code/contiki/idle_receiver/obj_avr-zigduino/rimestats.d
+++
@@ -1,5 +0,0 @@
-obj_avr-zigduino/rimestats.o: \
- /home/xavier/looci/looci2-contiki-os/contiki-2.7/core/net/rime/rimestats.c \
- /home/xavier/looci/looci2-contiki-os/contiki-2.7/core/net/rime/rimestats.h
- /home/xavier/looci/looci2-contiki-os/contiki-2.7/core/net/rime/rimestats.c :
- /home/xavier/looci/looci2-contiki-os/contiki-2.7/core/net/rime/rimestats.h :
--- code/contiki/idle_receiver/obj_avr-zigduino/stimer.d
+++
@@ -1,9 +0,0 @@
-obj_avr-zigduino/stimer.o: \
- /home/xavier/looci/looci2-contiki-os/contiki-2.7/core/sys/stimer.c \
- /home/xavier/looci/looci2-contiki-os/contiki-2.7/platform/avr-zigduino/contiki-conf.h \
- /home/xavier/looci/looci2-contiki-os/contiki-2.7/core/sys/clock.h \
- /home/xavier/looci/looci2-contiki-os/contiki-2.7/core/sys/stimer.h
- /home/xavier/looci/looci2-contiki-os/contiki-2.7/core/sys/stimer.c :
- /home/xavier/looci/looci2-contiki-os/contiki-2.7/platform/avr-zigduino/contiki-conf.h :
- /home/xavier/looci/looci2-contiki-os/contiki-2.7/core/sys/clock.h :
- /home/xavier/looci/looci2-contiki-os/contiki-2.7/core/sys/stimer.h :
