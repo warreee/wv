f188dd9422c3bfb16694634206f24892d8d075e1
Xavier Goás Aguililla <xavier.goas@gmail.com>
2015-05-03 19:37:57 +0200

    naive temp average done

--- code/contiki/naive_temp_average/obj_avr-zigduino/eeprom.d
+++
@@ -1,5 +0,0 @@
-obj_avr-zigduino/eeprom.o: \
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//cpu/avr/dev/eeprom.c \
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/dev/eeprom.h
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//cpu/avr/dev/eeprom.c :
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/dev/eeprom.h :
