f8cda6d4e2b4767505044983f1ab5ab251d0327b
Xavier Goás Aguililla <xavier.goas@gmail.com>
2015-05-04 11:58:48 +0200

    meer stuff, meer structuur

--- code/contiki/temp_filter/obj_avr-zigduino/leds-arch.d
+++
@@ -1,7 +0,0 @@
-obj_avr-zigduino/leds-arch.o: \
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//cpu/avr/./leds-arch.c \
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//platform/avr-zigduino/contiki-conf.h \
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/dev/leds.h
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//cpu/avr/./leds-arch.c :
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//platform/avr-zigduino/contiki-conf.h :
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/dev/leds.h :
--- code/contiki/temp_sense_n_send/obj_avr-zigduino/flash.d
+++
@@ -1,5 +0,0 @@
-obj_avr-zigduino/flash.o: \
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//cpu/avr/dev/flash.c \
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//cpu/avr/dev/flash.h
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//cpu/avr/dev/flash.c :
- /home/xavier/looci/looci2-contiki-os/contiki-2.7//cpu/avr/dev/flash.h :
