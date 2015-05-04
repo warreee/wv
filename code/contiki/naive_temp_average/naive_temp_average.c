fa6d58ff209c9042ec238e91013dd7167b26ac80
warreee <wardschodts@hotmail.com>
2015-05-04 22:19:53 +0200

    ram

--- code/contiki/naive_temp_average/naive_temp_average.c
+++ code/contiki/naive_temp_average/naive_temp_average.c
@@ -2,7 +2,7 @@
 
 #include "contiki.h"
 #include <avr/io.h>
-
+#include <stdio.h>
 /*---------------------------------------------------------------------------*/
 PROCESS(antenna_on_idle_process, "antenna on, idle");
 AUTOSTART_PROCESSES(&antenna_on_idle_process);
@@ -10,28 +10,33 @@ AUTOSTART_PROCESSES(&antenna_on_idle_process);
 PROCESS_THREAD(antenna_on_idle_process, ev, data)
 {
 #define AMOUNT_OF_SAMPLES 20
-#define AMOUNT_TO_AVERAGE 
-  static struct etimer et;
-  static uint8_t avg = 0;
-  static uint8_t temps[20] = {16,15,16,16,15,16,17,18,18,18,18,17,16,16,17,17,17,17,17,17};
 
+#define AMOUNT_TO_AVERAGE 5000
+  static struct etimer et;
+  static int avg;
+  static uint8_t temps[AMOUNT_TO_AVERAGE];
   PROCESS_BEGIN();
 
   // initialise pin
   DDRE |= _BV(PE6);
+    int i = 0;
+    for (i = 0; i < AMOUNT_TO_AVERAGE; i++) {
+      temps[i] = 16;
+    }
 
   while (1) {
     // flip pin
     PORTE ^= _BV(PE6);
     int i = 0;
-    for (i = 0; i < 20; i++) {
-      avg += temps[i];
+    for (i = 0; i < AMOUNT_TO_AVERAGE; i++) {
+      avg = avg + temps[i];
     }
 
-    avg = avg / 20;
+    avg = avg / AMOUNT_TO_AVERAGE;
+    //printf("average: %d",avg);
     // pin low
     PORTE &= ~(_BV(PE6));
-    etimer_set(&et, (CLOCK_SECOND * 0.5));
+    etimer_set(&et, (CLOCK_SECOND * 0.01));
 
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
   }
