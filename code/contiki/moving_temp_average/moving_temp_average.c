54f9b0aaff72ad4b384480670ffdb388a50690c7
Xavier Goás Aguililla <xavier.goas@gmail.com>
2015-05-04 23:12:59 +0200

    functie aangepast

--- code/contiki/moving_temp_average/moving_temp_average.c
+++ code/contiki/moving_temp_average/moving_temp_average.c
@@ -42,7 +42,7 @@ PROCESS_THREAD(antenna_on_idle_process, ev, data)
 
     //Pin laag
     PORTE &= ~(_BV(PE6));
-    etimer_set(&et, (CLOCK_SECOND * 0.1));
+    etimer_set(&et, (CLOCK_SECOND * 0.5));
 
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
   }
---
+++ paper/auto/bibliography.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook
+ "bibliography"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "titzer2005avrora"
+    "akyildiz2002wireless"
+    "mainwaring2002wireless"
+    "hughes2009looci"
+    "hughes2013energy")))
+
