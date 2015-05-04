c948afc71ee48e06e9173f7166da0768bbc424f6
Xavier Goás Aguililla <xavier.goas@gmail.com>
2015-05-04 11:58:32 +0200

    meer stuff, meer structuur

---
+++ code/contiki/temp_sense_n_send/obj_avr-zigduino/mmem.d
@@ -0,0 +1,9 @@
+obj_avr-zigduino/mmem.o: \
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/lib/mmem.c \
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/lib/mmem.h \
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/lib/list.h \
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//platform/avr-zigduino/contiki-conf.h
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/lib/mmem.c :
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/lib/mmem.h :
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//core/lib/list.h :
+ /home/xavier/looci/looci2-contiki-os/contiki-2.7//platform/avr-zigduino/contiki-conf.h :
