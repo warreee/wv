286b5824e9e585ea80155286f5fb3e006907f835
warreee <wardschodts@hotmail.com>
2015-05-05 04:20:01 +0200

    standard works

--- tool/main.py
+++ tool/main.py
@@ -10,6 +10,7 @@ class WAXA(cmd.Cmd):
     storage = 0.0
     time = 0.0
     transmissions = 0.0
+    bytes_per_transmission = 0.0
 
     # Variables used internally
     RAM_BYTE = 0.0000814375075478687 #mJ
@@ -18,7 +19,9 @@ class WAXA(cmd.Cmd):
     ANT_AMPERE = 0.04
     ANT_TIME_BYTE = 0.382 #ms
     ANT_TIME = 113.104 #ms
-    ANT_VOLT = 6
+    ANT_VOLT = 6.0
+
+    energy_standard = 0.0
 
 
     def do_exit(self, line):
@@ -28,9 +31,17 @@ class WAXA(cmd.Cmd):
         self.do_set_storage(line)
         self.do_set_execution_time(line)
         self.do_set_transmissions(line)
+        self.do_set_bytes_per_transmission(line)
+        self.do_calculate(line)
 
     def do_calculate(self, line):
-        print("Calculates the consumption en compares with standard consumption")
+        print("Calculates the consumption and compares with standard consumption")
+        standard = self.calculate_standard(line)
+        print("The standard energy usage is: " + standard)
+
+    def do_calculate_standard(self, line):
+        self.energy_standard = self.ANT_AMPERE * self.ANT_VOLT * (float(self.bytes_per_transmission) * self.ANT_TIME_BYTE + self.ANT_TIME)
+        print(self.energy_standard)
 
     def do_set_reduction(self, rdf):
         if not rdf:
@@ -68,6 +79,15 @@ class WAXA(cmd.Cmd):
         else:
             self.transmissions = transmissions
         print("The number of transmissions is set to: " + transmissions)
+
+    def do_set_bytes_per_transmission(self, bpt):
+        if not bpt:
+            bpt = input('Give the amount bytes/transmission: ')
+            self.bytes_per_transmission = bpt
+        else:
+            self.bytes_per_transmission = bpt
+        print("The number of bytes/transmissions is set to: " + bpt)
+
 
 
 if __name__ == '__main__':
