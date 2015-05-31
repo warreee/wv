99b174d93f877a587171764374f2c7f82d65e721
Xavier Goás Aguililla <xavier.goas@gmail.com>
2015-05-31 22:48:36 +0200

    tool uses config files now

--- tool/main.py
+++ tool/main.py
@@ -1,124 +1,95 @@
-import cmd
+import ConfigParser
 
-class WAXA(cmd.Cmd):
-    intro = "Ward & Xavier WSN commandline tool"
-    prompt = 'WAXA '
+# Are set by the user:
+reduction_factor = 0.0
+storage = 0.0
+time = 0.0
+transmissions = 0.0
+bytes_per_transmission = 0.0
 
-    # Are set by the user:
-    rdf = 0.0
-    storage = 0.0
-    time = 0.0
-    transmissions = 0.0
-    bytes_per_transmission = 0.0
+# Variables used internally
+RAM_BYTE = 0.0000814375075478687  #mJ
+CPU_AMPERE = 0.0037
+CPU_VOLT = 6.0
+ANT_AMPERE = 0.04
+ANT_TIME_BYTE = 0.382  #ms
+ANT_TIME_START = 113.104  #ms
+ANT_VOLT = 6.0
 
-    # Variables used internally
-    RAM_BYTE = 0.0000814375075478687  #mJ
-    CPU_AMPERE = 0.0037
-    CPU_VOLT = 6.0
-    ANT_AMPERE = 0.04
-    ANT_TIME_BYTE = 0.382  #ms
-    ANT_TIME_START = 113.104  #ms
-    ANT_VOLT = 6.0
+energy_standard = 0.0
+energy_RAM = 0.0
+energy_CPU = 0.0
+energy_ant = 0.0
+energy_tot = 0.0
+energy_efficiency = 0.0
 
-    energy_standard = 0.0
-    energy_RAM = 0.0
-    energy_CPU = 0.0
-    energy_ant = 0.0
-    energy_tot = 0.0
-    energy_efficiency = 0.0
+reduced_transmissions = 0
 
-    reduced_transmissions = 0
+def calculate():
+    calculate_standard()
+    calculate_improved()
+    print("Calculates the consumption and compares with standard consumption")
+    print("The standard energy usage is: " + str(energy_standard))
+    print("The new energy usage is: " + str(energy_tot))
+    energy_efficiency =  1.0 - energy_tot / energy_standard
+    print("If you deploy locally on the mote the component is " + str(energy_efficiency * 100.0) +"% more efficient!")
 
+def calculate_standard():
+    energy_ant = ANT_AMPERE * ANT_VOLT * (float(bytes_per_transmission) * ANT_TIME_BYTE + ANT_TIME_START)
+    global energy_standard
+    energy_standard = float(energy_ant) * float(transmissions)
+    # print(energy_standard)
 
-    def do_exit(self, line):
-        raise SystemExit
+def calculate_improved():
+    energy_RAM = RAM_BYTE * float(storage)
+    energy_CPU = CPU_AMPERE * float(time) * CPU_VOLT
+    energy_ant = ANT_AMPERE * ANT_VOLT * (float(bytes_per_transmission) * ANT_TIME_BYTE + ANT_TIME_START)
+    global reduced_transmissions
+    reduced_transmissions = int(transmissions) / int(reduction_factor)
+    global energy_tot
+    energy_tot = energy_CPU + energy_RAM  + (energy_ant * reduced_transmissions)
 
-    def do_deploy(self, line):
-        self.do_set_reduction(line)
-        self.do_set_storage(line)
-        self.do_set_execution_time(line)
-        self.do_set_transmissions(line)
-        self.do_set_bytes_per_transmission(line)
-        self.do_calculate(line)
+def set_reduction(rdf):
+    # rdf = input('Give the reduction factor: ')
+    global reduction_factor
+    reduction_factor = rdf
+    print("The reduction factor is set to: " + str(rdf))
 
-    def do_calculate(self, line):
-        self.do_calculate_standard(line)
-        self.do_calculate_opt(line)
-        print("Calculates the consumption and compares with standard consumption")
-        print("The standard energy usage is: " + str(self.energy_standard))
-        print("The new energy usage is: " + str(self.energy_tot))
-        self.energy_efficiency =  1.0 - self.energy_tot / self.energy_standard
-        print("If you deploy here the component is " + str(self.energy_efficiency * 100.0) +"% more efficient!")
+def set_storage(stor):
+    # storage = input('Give the amount of stored bytes: ')
+    global storage
+    storage = stor
+    print("The number of bytes stored is set to: " + str(storage))
 
+def set_execution_time(tm):
+    # time = input('Give the execution time of CPU to do computations: ')
+    global time
+    time = tm
+    print("The execution time of the calculation is set to: " + str(time))
 
-    def do_calculate_standard(self, line):
-        self.calculate_ANT()
-        self.energy_standard = float(self.energy_ant) * float(self.transmissions)
-        print(self.energy_standard)
+def set_transmissions(tms):
+    # transmissions = input('Give the amount transmission/time_unit: ')
+    global transmissions
+    transmissions = tms
+    print("The number of transmissions is set to: " + str(transmissions))
 
-    def do_calculate_opt(self, line):
-        self.calculate_RAM()
-        self.calculate_CPU()
-        self.calculate_ANT()
-        self.reduced_transmissions = int(self.transmissions) / int(self.rdf)
-        self.energy_tot = self.energy_CPU + self.energy_RAM  + (self.energy_ant * self.reduced_transmissions)
-
-    def calculate_RAM(self):
-        self.energy_RAM = self.RAM_BYTE * float(self.storage)
-
-    def calculate_CPU(self):
-        self.energy_CPU = self.CPU_AMPERE * float(self.time) * self.CPU_VOLT
-
-    def calculate_ANT(self):
-        self.energy_ant = self.ANT_AMPERE * self.ANT_VOLT * (
-            float(self.bytes_per_transmission) * self.ANT_TIME_BYTE + self.ANT_TIME_START)
-
-
-    def do_set_reduction(self, rdf):
-        if not rdf:
-            rdf = input('Give the reduction factor: ')
-            self.rdf = rdf
-        else:
-            self.rdf = rdf
-        print("The reduction factor is set to: " + rdf)
-
-    def help_set_reduction(self):
-        print('\n'.join(['set_reduction [reduction factor]',
-                         'Sets the reduction factor',
-        ]))
-
-    def do_set_storage(self, storage):
-        if not storage:
-            storage = input('Give the amount of stored bytes: ')
-            self.storage = storage
-        else:
-            self.storage = storage
-        print("The number of bytes stored is set to: " + storage)
-
-    def do_set_execution_time(self, time):
-        if not time:
-            time = input('Give the execution time of CPU to do computations: ')
-            self.time = time
-        else:
-            self.time = time
-        print("The execution time of the calculation is set to: " + time)
-
-    def do_set_transmissions(self, transmissions):
-        if not transmissions:
-            transmissions = input('Give the amount transmission/time_unit: ')
-            self.transmissions = transmissions
-        else:
-            self.transmissions = transmissions
-        print("The number of transmissions is set to: " + transmissions)
-
-    def do_set_bytes_per_transmission(self, bpt):
-        if not bpt:
-            bpt = input('Give the amount bytes/transmission: ')
-            self.bytes_per_transmission = bpt
-        else:
-            self.bytes_per_transmission = bpt
-        print("The number of bytes/transmissions is set to: " + bpt)
-
+def set_bytes_per_transmission(bpt):
+    # bpt = input('Give the amount bytes/transmission: ')
+    global bytes_per_transmission
+    bytes_per_transmission = bpt
+    print("The number of bytes/transmission is set to: " + str(bpt))
 
 if __name__ == '__main__':
-    WAXA().cmdloop()
+    config = ConfigParser.RawConfigParser()
+    config.read('parameters.cfg')
+    # print config.get('sensor', 'sensor_data_size')
+    set_reduction(int(config.get('reduction', 'transmit_reduction')))
+    if config.get('memory', 'buffer_used') == True:
+        set_storage(float(config.get('memory', 'buffer_size')))
+    else:
+        set_storage(0)
+    set_execution_time(config.get('frequency', 'reduction_cpu_time'))
+    set_transmissions(int(config.get('frequency', 'transmission_frequency')))
+    set_bytes_per_transmission(int(float(config.get('sensor', 'sensor_data_size')) / float(config.get('reduction', 'data_reduction'))))
+    calculate()
+    raise SystemExit
